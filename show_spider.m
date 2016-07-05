584c13f1499cd63dbbae4397e78161e9cd741021
karkuspeter <karkuspeter@gmail.com>
2016-07-05 15:00:39 +0800

    show plan type figure for repeat results

--- show_spider.m
+++ show_spider.m
@@ -11,6 +11,11 @@ plot_confidence((1:size(rep_stats.mean_linstat.theta_mu,1))', rep_stats.mean_lin
 xlabel('Iteration')
 ylabel('slip(theta_mu) (mean of slip function)')
 
+figure()
+plot_confidence((1:size(rep_stats.mean_linstat.plan_type,1))', rep_stats.mean_linstat.plan_type(:,1), rep_stats.std_linstat.plan_type(:,1));
+xlabel('Iteration')
+ylabel('Bridge plan used');
+
 % figure()
 % plot_confidence((1:size(w_mean,1))', w_mean(:,2), w_std(:,2));
 % xlabel('Iteration')
